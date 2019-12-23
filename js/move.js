613bc458aab3c7ffa01f7712372c8302a7f8639e
cy857213312 <35824619+cy857213312@users.noreply.github.com>
2019-12-23 22:47:38 +0800

    firstcommit

---
+++ js/move.js
@@ -0,0 +1,9 @@
+function openDialog() {
+    document.getElementById('light').style.display = 'block';
+
+}
+
+function closeDialog() {
+    document.getElementById('light').style.display = 'none';
+
+}
