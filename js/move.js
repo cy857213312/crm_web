bc413f29da15034cd572a5316b16ad1099dc7835
cy857213312 <35824619+cy857213312@users.noreply.github.com>
2020-01-09 21:12:45 +0800

    将task 模块与接口连接

--- js/move.js
+++ js/move.js
@@ -3,7 +3,16 @@ function openDialog() {
 
 }
 
+
 function closeDialog() {
     document.getElementById('light').style.display = 'none';
+
+}
+
+
+
+
+function closeDialogU() {
+    document.getElementById('lightU').style.display = 'none';
 
 }
